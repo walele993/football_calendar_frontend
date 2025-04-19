b9ef5a29f9bc64ca388efc84f33c15ff7dd506fd
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-19 12:27:07 +0200

    Solved directory issue

--- app/src/main/java/com/walele/footballcalendarapp/ui/theme/Color.kt
+++ app/src/main/java/com/walele/footballcalendarapp/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.walele.footballcalendarapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme colors
+val backgroundLight = Color(0xFFFFFFFF) // White
+val primaryLight = Color(0xFF6200EE) // Blue
+val secondaryLight = Color(0xFF03DAC5) // Teal
+val accentLight = Color(0xFFFF5722) // Deep Orange
+val onBackgroundLight = Color(0xFF000000) // Black
+val onPrimaryLight = Color(0xFFFFFFFF) // White
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme colors
+val backgroundDark = Color(0xFF121212) // Dark Gray
+val primaryDark = Color(0xFFBB86FC) // Light Purple
+val secondaryDark = Color(0xFF03DAC6) // Teal
+val accentDark = Color(0xFFFF5722) // Deep Orange
+val onBackgroundDark = Color(0xFFFFFFFF) // White
+val onPrimaryDark = Color(0xFF000000) // Black
