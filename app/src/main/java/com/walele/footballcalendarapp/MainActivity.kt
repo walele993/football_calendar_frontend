15627e4168a5801ecffcc642d958f71b8bc729f0
walele993 <64219379+walele993@users.noreply.github.com>
2025-04-19 11:01:54 +0200

    Initial Android project

---
+++ app/src/main/java/com/walele/footballcalendarapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.walele.footballcalendar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.walele.footballcalendar.ui.theme.FootballCalendarAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FootballCalendarAppTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White), // sfondo bianco
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Football Calendar App",
+            style = MaterialTheme.typography.headlineLarge,
+            color = Color(0xFFFA574F) // accento rosso-arancio
+        )
+    }
+}
